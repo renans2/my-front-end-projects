81737d01e5482138eb4530f8910b6f19125c1f6a
Renan Silva <renansupermanox@gmail.com>
2024-05-17 22:52:12 +0100

    more of clock

--- clock/script.js
+++ clock/script.js
@@ -1,27 +1,32 @@
-let hours   = 0;
-let minutes = 0;
-let seconds = 0;
+let currentDate = new Date();
+let hours   = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+
+const displayText = $("#display-text");
 
 setInterval(update, 1000);
 
 function update(){
-    //console.log(hours + ":" + minutes + ":" + seconds);
-    changeDisplay();
     incSeconds();
+    changeDisplay();
 }
 
 function incSeconds(){
     seconds = (seconds + 1) % 60;
-
     if(seconds == 0){
         minutes = (minutes + 1) % 60;
-        
         if(minutes == 0)
-            hours++;
+            hours = (hours + 1) % 24;
     }
 }
 
 function changeDisplay(){
-    $("#display-text").text(hours + ":" + minutes + ":" + seconds);
-    console.log(hours + ":" + minutes + ":" + seconds);
+    displayText.text(getFormattedTime());
+}
+
+function getFormattedTime(){
+    return hours  .toString().padStart(2, "0") + ":" + 
+           minutes.toString().padStart(2, "0") + ":" + 
+           seconds.toString().padStart(2, "0");
 }
