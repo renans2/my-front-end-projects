fd77b3147242dee6ade45161d041473fa5f948c3
Renan Silva <renansupermanox@gmail.com>
2024-06-07 20:03:46 +0100

    toggle code improved

--- src/toggle/script.js
+++ src/toggle/script.js
@@ -1,44 +1,16 @@
-let toggleOn = false;
-$("body").addClass("light-off");
-$("#toggle-circle").addClass("off");
-$(".toggle-background").addClass("background-off");
-$("#toggle-me").addClass("white-text");
+const body = $("body");
+const toggleCircle = $("#toggle-circle");
+const toggleBackgroud = $(".toggle-background");
+const toggleMeText = $("#toggle-me-text");
 
-$(".toggle").on("click", toggle);
+body.addClass("light-off");
+toggleCircle.addClass("off");
+toggleBackgroud.addClass("background-off");
+toggleMeText.addClass("white-text");
 
-
-function toggle(){
-    if(toggleOn) turnOff();
-    else         turnOn();
-
-    toggleOn = !toggleOn;
-    changeLightStateAndTextColor();
-}
-
-function turnOn(){
-    $("#toggle-circle").removeClass("off");
-    $("#toggle-circle").addClass("on");
-    $(".toggle-background").removeClass("background-off");
-    $(".toggle-background").addClass("background-on");
-}
-
-function turnOff(){
-    $("#toggle-circle").removeClass("on");
-    $("#toggle-circle").addClass("off");
-    $(".toggle-background").removeClass("background-on");
-    $(".toggle-background").addClass("background-off");
-}
-
-function changeLightStateAndTextColor(){
-    if(toggleOn){
-        $("body").removeClass("light-off");
-        $("body").addClass("light-on");
-        $("#toggle-me").removeClass("white-text");
-        $("#toggle-me").addClass("black-text");
-    }else{
-        $("body").removeClass("light-on");
-        $("body").addClass("light-off");
-        $("#toggle-me").removeClass("black-text");
-        $("#toggle-me").addClass("white-text");
-    }
-}
+$(".toggle").on("click", function(){
+    toggleCircle.toggleClass("off on");
+    toggleBackgroud.toggleClass("background-off background-on");
+    body.toggleClass("light-off light-on");
+    toggleMeText.toggleClass("white-text black-text");
+});
