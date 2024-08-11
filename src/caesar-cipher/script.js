4c4813c915c38cd5c8172dd642dbef88f6d6d568
Renan Silva <renansupermanox@gmail.com>
2024-08-11 11:17:38 +0100

    caesar cipher done

--- src/caesar-cipher/script.js
+++ src/caesar-cipher/script.js
@@ -2,27 +2,51 @@
  * @author Renan Silva (renans2 on GitHub)
  */
 
+const typeSelect = $("#type-select");
+const cipherButton = $("#cipher-button");
+
 for (let i = 0; i < 26; i++) {
     $("#shift-amount-select").append(`<option value=${i}>${i}</option>`);
 }
 
-$("#cipher-button").on("click", function (){
+typeSelect.on("change", function(){
+    if(typeSelect.val() === "encrypt") {
+        cipherButton.html("Encrypt");
+        $("label[for='input']").html("Text to be encrypted:");
+        $("label[for='output']").html("Encrypted text:");
+    } else {
+        cipherButton.html("Decrypt");
+        $("label[for='input']").html("Text to be decrypted:");
+        $("label[for='output']").html("Decrypted text:");
+    }
+});
+
+cipherButton.on("click", function (){
    const input = $("#input").val();
    const shiftAmount = parseInt($("#shift-amount-select").val());
-   const shiftDirection = $("#shift-direction-select").val();
+   const type = typeSelect.val();
 
-   if(shiftDirection === "left")
-       $("#output").html(cipherInput(input, 26 - shiftAmount));
-   else
+   if(type === "encrypt")
        $("#output").html(cipherInput(input, shiftAmount));
+   else
+       $("#output").html(cipherInput(input, 26 - shiftAmount));
 });
 
 function cipherInput(input, shift){
     let result = "";
 
     for (let i = 0; i < input.length; i++) {
-        const shiftedChar = String.fromCharCode((((input.charCodeAt(i) % 97) + shift) % 26) + 97);
-        result += shiftedChar;
+        const charCode = input.charCodeAt(i);
+
+        if(65 <= charCode && charCode <= 90){ // Uppercase letters
+            const shiftedChar = String.fromCharCode((((charCode % 65) + shift) % 26) + 65);
+            result += shiftedChar;
+        } else if(97 <= charCode && charCode <= 122){ // Lowercase letters
+            const shiftedChar = String.fromCharCode((((charCode % 97) + shift) % 26) + 97);
+            result += shiftedChar;
+        } else {
+            result += input.charAt(i);
+        }
     }
 
     return result
