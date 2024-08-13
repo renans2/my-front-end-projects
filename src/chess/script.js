d9d2d1cf1287554b5184a057c72b92413f9d5358
Renan Silva <renansupermanox@gmail.com>
2024-08-13 19:14:23 +0100

    more of chess

--- src/chess/script.js
+++ src/chess/script.js
@@ -30,43 +30,156 @@ function fillBoardWithDivs(){
 function setStartingPositions(){
     // Pawns
     for (let j = 0; j < 8; j++){
-        $(board[1][j]).addClass("pawn-black black-piece");
-        $(board[6][j]).addClass("pawn-white white-piece");
+        $(board[1][j]).attr("data-piece", "pawn-black");
+        $(board[1][j]).addClass("black-piece");
+        $(board[0][j]).addClass("black-piece");
+        $(board[6][j]).attr("data-piece", "pawn-white");
+        $(board[6][j]).addClass("white-piece");
+        $(board[7][j]).addClass("white-piece");
     }
 
     // Rooks
-    $(board[0][0]).addClass("rook-black black-piece");
-    $(board[0][7]).addClass("rook-black black-piece");
-    $(board[7][0]).addClass("rook-white white-piece");
-    $(board[7][7]).addClass("rook-white white-piece");
+    $(board[0][0]).attr("data-piece", "rook-black");
+    $(board[0][7]).attr("data-piece", "rook-black");
+    $(board[7][0]).attr("data-piece", "rook-white");
+    $(board[7][7]).attr("data-piece", "rook-white");
 
     // Knights
-    $(board[0][1]).addClass("knight-black black-piece");
-    $(board[0][6]).addClass("knight-black black-piece");
-    $(board[7][1]).addClass("knight-white white-piece");
-    $(board[7][6]).addClass("knight-white white-piece");
+    $(board[0][1]).attr("data-piece", "knight-black");
+    $(board[0][6]).attr("data-piece", "knight-black");
+    $(board[7][1]).attr("data-piece", "knight-white");
+    $(board[7][6]).attr("data-piece", "knight-white");
 
     // Bishops
-    $(board[0][2]).addClass("bishop-black black-piece");
-    $(board[0][5]).addClass("bishop-black black-piece");
-    $(board[7][2]).addClass("bishop-white white-piece");
-    $(board[7][5]).addClass("bishop-white white-piece");
+    $(board[0][2]).attr("data-piece", "bishop-black");
+    $(board[0][5]).attr("data-piece", "bishop-black");
+    $(board[7][2]).attr("data-piece", "bishop-white");
+    $(board[7][5]).attr("data-piece", "bishop-white");
 
     // Queens
-    $(board[0][3]).addClass("queen-black black-piece");
-    $(board[7][3]).addClass("queen-white white-piece");
+    $(board[0][3]).attr("data-piece", "queen-black");
+    $(board[7][3]).attr("data-piece", "queen-white");
 
     // Kings
-    $(board[0][4]).addClass("king-black black-piece");
-    $(board[7][4]).addClass("king-white white-piece");
+    $(board[0][4]).attr("data-piece", "king-black");
+    $(board[7][4]).attr("data-piece", "king-white");
 }
 
 $(".white-piece").on("click", function(){
    if(turn === 0){
-       const i = $(this).attr("data-i");
-       const j = $(this).attr("data-j");
+       const i     = parseInt($(this).attr("data-i"));
+       const j     = parseInt($(this).attr("data-j"));
+       const piece = $(this).attr("data-piece");
+
+       let options = highlightAndReturnOptions(piece, i, j);
+       console.log(options);
+       console.log(i);
    }
 });
+
+// $(".black").on("click", function(){
+//     if(turn === 1){
+//         const i     = parseInt($(this).attr("data-i"));
+//         const j     = parseInt($(this).attr("data-j"));
+//         const piece = $(this).attr("data-piece");
+//
+//         let options = highlightAndReturnOptions(piece, i, j);
+//         console.log(options);
+//         console.log(i);
+//     }
+// });
+
+function highlightAndReturnOptions(piece, i, j){
+    switch(piece){
+        case "pawn-black"  : return casePawn(i, j, "black");
+        case "pawn-white"  : return casePawn(i, j, "white");
+        case "rook-black"  : return caseRook(i, j, "black");
+        case "rook-white"  : return caseRook(i, j, "white");
+        case "bishop-black": return caseBishop(i, j, "black");
+        case "bishop-white": return caseBishop(i, j, "white");
+        case "queen-black" : return caseQueen(i, j, "black");
+        case "queen-white" : return caseQueen(i, j, "white");
+        case "king-black"  : return caseKing(i, j, "black");
+        case "king-white"  : return caseKing(i, j, "white");
+    }
+}
+
+function casePawn(i, j, color){
+    let options = [];
+
+    if(color === "white" && i > 0){
+        if(j-1 >= 0 && $(board[i-1][j-1]).hasClass("black-piece")){
+            options.push({
+                i: i-1,
+                j: j-1
+            });
+        }
+        if(j+1 <= 7 && $(board[i-1][j+1]).hasClass("black-piece")){
+            options.push({
+                i: i-1,
+                j: j+1
+            });
+        }
+        if(!$(board[i-1][j]).hasClass("black-piece white-piece")){
+            options.push({
+                i: i-1,
+                j: j
+            });
+        }
+    } else if(color === "black" && i < 7){
+        if(j-1 >= 0 && $(board[i+1][j-1]).hasClass("white-piece")){
+            options.push({
+                i: i+1,
+                j: j-1
+            });
+        }
+        if(j+1 <= 7 && $(board[i+1][j+1]).hasClass("white-piece")){
+            options.push({
+                i: i+1,
+                j: j+1
+            });
+        }
+        if($(board[i+1][j]).hasClass("black-piece white-piece")){
+            options.push({
+                i: i+1,
+                j: j
+            });
+        }
+    }
+
+    return options;
+}
+
+function caseRook(i, j, color){
+
+}
+
+function caseBishop(i, j, color){
+
+}
+
+function caseQueen(i, j, color){
+
+}
+
+function caseKing(i, j, color){
+    let options = [];
+
+    for (let k = i-1; k < i+1; k++)
+        for (let l = j-1; l < j+1; l++)
+            if((k !== i || l !== j) &&
+                0 <= k && k <= 7    &&
+                0 <= l && l <= 7){
+                if(color === "white" && !$(board[k][l]).hasClass("white-piece") ||
+                   color === "black" && !$(board[k][l]).hasClass("black-piece"))
+                    options.push({
+                        i: k,
+                        j: l
+                    });
+            }
+
+    return options;
+}
 
 // $(".square").each(function(){
 //     const i = $(this).attr("data-i");
